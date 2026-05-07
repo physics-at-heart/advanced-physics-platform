5cb4572d77f4a0db99f64cab040f84bf490b9404
physics-at-heart <grouptheai@gmail.com>
2026-05-08 02:51:37 +0300

    Create modules.js

---
+++ client/src/data/modules.js
@@ -0,0 +1,5 @@
+export const modules = [
+  { id: 'module1', title: 'Module 1: Energy, Work and Machines', topics: ['Forms of Energy', 'Work Done', 'Power', 'Efficiency', 'Simple Machines', 'Conservation of Energy'], duration: '6 weeks', image: 'https://images.unsplash.com/photo-1518531933037-91b2f5f229cc' },
+  { id: 'module2', title: 'Module 2: Thermal Physics and Heat Transfer', topics: ['Temperature', 'Heat Energy', 'Conduction', 'Convection', 'Radiation', 'Expansion of Solids and Liquids'], duration: '6 weeks', image: 'https://images.unsplash.com/photo-1532094349884-543bc11b234d' },
+  { id: 'module3', title: 'Module 3: Modern Physics and Space Science', topics: ['Atomic Structure', 'Radioactivity', 'Electromagnetic Waves', 'Satellites', 'Solar System', 'Space Exploration'], duration: '6 weeks', image: 'https://images.unsplash.com/photo-1446776811953-b23d57bd21aa' },
+];
