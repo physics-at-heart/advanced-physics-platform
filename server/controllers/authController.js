089922f7de7ef3c72d17fd3c279916469b6b1dba
physics-at-heart <grouptheai@gmail.com>
2026-05-08 02:39:25 +0300

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+    const user = await User.create({ name, email, password: hashed, role });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    res.status(201).json({ token, user: { id: user._id, name, email, role } });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    res.json({ token, user: { id: user._id, name: user.name, email, role: user.role } });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+};
+
+const getMe = async (req, res) => {
+  res.json(req.user);
+};
+
+module.exports = { register, login, getMe };
