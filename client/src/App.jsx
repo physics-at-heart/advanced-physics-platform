227a70c62986516e427638f2a130f63554d31712
physics-at-heart <grouptheai@gmail.com>
2026-05-08 02:48:22 +0300

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/common/ScrollToTop';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <AppRoutes />
+    </Router>
+  );
+}
+export default App;
