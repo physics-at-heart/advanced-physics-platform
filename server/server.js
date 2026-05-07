533c95440ea78c0f911c275c245ed111659ae3e8
physics-at-heart <grouptheai@gmail.com>
2026-05-08 02:34:58 +0300

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const moduleRoutes = require('./routes/moduleRoutes');
+const quizRoutes = require('./routes/quizRoutes');
+const progressRoutes = require('./routes/progressRoutes');
+const resourceRoutes = require('./routes/resourceRoutes');
+const { errorHandler } = require('./middleware/errorHandler');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/modules', moduleRoutes);
+app.use('/api/quizzes', quizRoutes);
+app.use('/api/progress', progressRoutes);
+app.use('/api/resources', resourceRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
