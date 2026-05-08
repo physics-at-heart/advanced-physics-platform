8eb28a51d4e51c0a80795ea59c6d2b74d9e529f2
physics-at-heart <grouptheai@gmail.com>
2026-05-08 04:44:14 +0300

    Update resourceController.js

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -1,7 +1,10 @@
-const Resource = require('../models/Resource');
+const resources = [
+  { _id: '1', title: 'Physics Formula Sheet', type: 'PDF', url: '#' },
+  { _id: '2', title: 'Energy Notes', type: 'PDF', url: '#' },
+  { _id: '3', title: 'Thermal Physics Guide', type: 'PDF', url: '#' }
+];
 
 const getResources = async (req, res) => {
-  const resources = await Resource.find();
   res.json(resources);
 };
 
