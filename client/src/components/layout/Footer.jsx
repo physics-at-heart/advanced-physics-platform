ee96b343847cc76c41d4044eba043a76c1ec815c
physics-at-heart <grouptheai@gmail.com>
2026-05-08 13:37:39 +0300

    Update Footer.jsx

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => (
   <footer className="bg-dark text-white py-4 mt-5" style={{ backgroundColor: 'var(--primary-deep-navy)' }}>
     <div className="container text-center">
       <p className="mb-1">&copy;  2026 Advanced Secondary Physics Learning Platform</p>
-      <small>Designed and developed by Adyamo Kevin, Namata Joseline, Ntabaazi Jonathan Lyazi, Ariokot Genevieve, Nakisige Hilda Patience.</small>
+      <small>Designed and developed by Group 5 </small>
     </div>
   </footer>
 );
